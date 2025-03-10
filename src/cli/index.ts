a8635163ec10ae0e523e14b20a9cdb27beee67de
newexpand <magin0auto@gmail.com>
2025-03-10 10:18:40 +0900

    feat(commit): 커밋 후 자동으로 푸시 기능 추가
    커밋 명령어에 자동 푸시 옵션을 추가하였습니다.
    -  옵션 사용 시 자동으로 푸시옵션 활성화
    - 현재 브랜치 정보 가져오는 기능 추가
    - 원격 저장소에 자동으로 푸시하는 기능 추가
    - 병합 관련 디버그 메시지 제거 및 가독성 향상

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -70,17 +70,17 @@ async function main() {
       .description(t("commands.commit.description"))
       .argument("[subcommand]", t("commands.commit.args.subcommand"))
       .argument("[message]", t("commands.commit.args.message"))
-      .option("-a, --all", t("commands.commit.options.all"))
+      .option("-a, --all", t("commands.commit.options.all_with_push"))
       .option("-p, --patch", t("commands.commit.options.patch"))
       .addHelpText(
         "after",
         `
         ${t("commands.commit.help.examples")}:
           $ autopr commit                    - ${t("commands.commit.help.default")}
-          $ autopr commit -a                 - ${t("commands.commit.help.all")}
+          $ autopr commit -a                 - ${t("commands.commit.help.all_with_push")}
           $ autopr commit improve            - ${t("commands.commit.help.improve_last")}
           $ autopr commit improve "message"  - ${t("commands.commit.help.improve_message")}
-          $ autopr commit improve -a         - ${t("commands.commit.help.improve_all")}
+          $ autopr commit improve -a         - ${t("commands.commit.help.improve_all_with_push")}
       `,
       )
       .action(commitCommand);
